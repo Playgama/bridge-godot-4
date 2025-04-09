1f56375b86f39a0236aac96525c0970b5a772a49
Sergei <sergei@playgama.com>
2025-04-09 09:22:29 +0300

    Move SDK settings to JSON file

--- addons/playgama_bridge/postprocessor.gd
+++ addons/playgama_bridge/postprocessor.gd
@@ -3,16 +3,9 @@ extends EditorExportPlugin
 const INDEX_FILE_NAME = "index.html"
 const TMP_JS_EXPORT_INDEX_FILE_NAME = "tmp_js_export.html"
 const JS_SDK_FILE_NAME = "playgama-bridge.js"
-const JS_SDK_PATH = "res://addons/playgama_bridge/template/" + JS_SDK_FILE_NAME
-const SETTINGS_PATH = "addons/playgama_bridge/general/"
-const SETTINGS_GAME_DISTRIBUTION_GAME_ID_KEY = "game_distribution_game_id"
-const SETTINGS_Y8_GAME_ID_KEY = "y8_game_id"
-const SETTINGS_Y8_CHANNEL_ID_KEY = "y8_channel_id"
-const SETTINGS_Y8_ADSENSE_ID_KEY = "y8_adsense_id"
-const SETTINGS_LAGGED_DEV_ID_KEY = "lagged_dev_id"
-const SETTINGS_LAGGED_PUBLISHER_ID_KEY = "lagged_publisher_id"
-const SETTINGS_FACEBOOK_PLACEMENT_ID_KEY = "facebook_placement_id"
-const SETTINGS_ADSGRAM_BLOCK_ID_KEY = "adsgram_block_id"
+const JS_SDK_CONFIG_FILE_NAME = "playgama-bridge-config.json"
+const JS_SDK_PATH = "res://addons/playgama_bridge/template/"
+
 
 var _path = null
 
@@ -23,64 +16,13 @@ func _export_begin(features, is_debug, path, flags):
 	if features.has("web"):
 		_path = path
 		
-		var file_from = FileAccess.open(JS_SDK_PATH, FileAccess.READ)
-		
-		var file_to = FileAccess.open(path.get_base_dir() + "/" + JS_SDK_FILE_NAME, FileAccess.WRITE)
-		file_to.store_string(file_from.get_as_text())
-		
-		file_from = null
-		file_to = null
+		_copy_file(JS_SDK_FILE_NAME)
+		_copy_file(JS_SDK_CONFIG_FILE_NAME)
 
-func _export_end():
-	if _path == null:
-		return
+func _copy_file(file_name):
+	var file_from = FileAccess.open(JS_SDK_PATH + file_name, FileAccess.READ)
+	var file_to = FileAccess.open(_path.get_base_dir() + "/" + file_name, FileAccess.WRITE)
+	file_to.store_string(file_from.get_as_text())
 	
-	var game_distribution_game_id = ""
-	var y8_game_id = ""
-	var y8_channel_id = ""
-	var y8_adsense_id = ""
-	var lagged_dev_id = ""
-	var lagged_publisher_id = ""
-	var facebook_placement_id = ""
-	var adsgram_block_id = ""
-	
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_GAME_DISTRIBUTION_GAME_ID_KEY):
-		game_distribution_game_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_GAME_DISTRIBUTION_GAME_ID_KEY)
-	
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_Y8_GAME_ID_KEY):
-		y8_game_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_Y8_GAME_ID_KEY)
-	
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_Y8_CHANNEL_ID_KEY):
-		y8_channel_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_Y8_CHANNEL_ID_KEY)
-
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_Y8_ADSENSE_ID_KEY):
-		y8_adsense_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_Y8_ADSENSE_ID_KEY)
-
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_LAGGED_DEV_ID_KEY):
-		lagged_dev_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_LAGGED_DEV_ID_KEY)
-
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_LAGGED_PUBLISHER_ID_KEY):
-		lagged_publisher_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_LAGGED_PUBLISHER_ID_KEY)
-
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_FACEBOOK_PLACEMENT_ID_KEY):
-		facebook_placement_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_FACEBOOK_PLACEMENT_ID_KEY)
-	
-	if ProjectSettings.has_setting(SETTINGS_PATH + SETTINGS_ADSGRAM_BLOCK_ID_KEY):
-		adsgram_block_id = ProjectSettings.get(SETTINGS_PATH + SETTINGS_ADSGRAM_BLOCK_ID_KEY)
-	
-	var index = FileAccess.open(_path.get_base_dir() + "/" + INDEX_FILE_NAME, FileAccess.READ_WRITE)
-	if not index:
-		index = FileAccess.open(_path.get_base_dir() + "/" + TMP_JS_EXPORT_INDEX_FILE_NAME, FileAccess.READ_WRITE)
-	
-	var content = index.get_as_text()
-	content = content.format({"game_distribution_game_id":game_distribution_game_id})
-	content = content.format({"y8_game_id":y8_game_id})
-	content = content.format({"y8_channel_id":y8_channel_id})
-	content = content.format({"y8_adsense_id":y8_adsense_id})
-	content = content.format({"lagged_dev_id":lagged_dev_id})
-	content = content.format({"lagged_publisher_id":lagged_publisher_id})
-	content = content.format({"facebook_placement_id":facebook_placement_id})
-	content = content.format({"adsgram_block_id":adsgram_block_id})
-	
-	index.store_string(content)
-	index = null
+	file_from = null
+	file_to = null
