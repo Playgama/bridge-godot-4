2f6169a2200e5a5f41353135dcd6b52f1333bbbe
Davit <davitsedrakian@gmail.com>
2026-06-15 14:59:16 +0400

    minor fixes

--- addons/playgama_bridge/modules/achievements/achievements.gd
+++ addons/playgama_bridge/modules/achievements/achievements.gd
@@ -1,16 +1,8 @@
 var is_supported : get = _is_supported_getter
-var is_get_list_supported : get = _is_get_list_supported_getter
-var is_native_popup_supported : get = _is_native_popup_supported_getter
 
 func _is_supported_getter():
 	return _js_achievements.isSupported
 
-func _is_get_list_supported_getter():
-	return _js_achievements.isGetListSupported
-
-func _is_native_popup_supported_getter():
-	return _js_achievements.isNativePopupSupported
-	
 var _js_achievements = null
 var _unlock_callback = null
 var _js_unlock_then = JavaScriptBridge.create_callback(self._on_js_unlock_then)
@@ -18,9 +10,6 @@ var _js_unlock_catch = JavaScriptBridge.create_callback(self._on_js_unlock_catch
 var _get_list_callback = null
 var _js_get_list_then = JavaScriptBridge.create_callback(self._on_js_get_list_then)
 var _js_get_list_catch = JavaScriptBridge.create_callback(self._on_js_get_list_catch)
-var _show_native_popup_callback = null
-var _js_show_native_popup_then = JavaScriptBridge.create_callback(self._on_js_show_native_popup_then)
-var _js_show_native_popup_catch = JavaScriptBridge.create_callback(self._on_js_show_native_popup_catch)
 func unlock(id, callback = null):
 	if _unlock_callback != null:
 		return
@@ -36,15 +25,6 @@ func get_list(callback = null):
 	_get_list_callback = callback
 
 	_js_achievements.getList().then(_js_get_list_then).catch(_js_get_list_catch)
-
-func show_native_popup(callback = null):
-	if _show_native_popup_callback != null:
-		return
-
-	_show_native_popup_callback = callback
-
-	_js_achievements.showNativePopup().then(_js_show_native_popup_then).catch(_js_show_native_popup_catch)
-
 
 func _init(js_achievements):
 	_js_achievements = js_achievements
@@ -83,13 +63,3 @@ func _on_js_get_list_catch(args):
 	if _get_list_callback != null:
 		_get_list_callback.call(false, [])
 		_get_list_callback = null
-
-func _on_js_show_native_popup_then(args):
-	if _show_native_popup_callback != null:
-		_show_native_popup_callback.call(true)
-		_show_native_popup_callback = null
-
-func _on_js_show_native_popup_catch(args):
-	if _show_native_popup_callback != null:
-		_show_native_popup_callback.call(false)
-		_show_native_popup_callback = null
--- examples/scenes/achievements.gd
+++ examples/scenes/achievements.gd
@@ -3,15 +3,11 @@ extends DetailedSceneBase
 
 
 @onready var is_achievements_supported_label = $MarginContainer2/VBoxContainer/IsAchievementsSupported
-@onready var is_get_list_supported_label = $MarginContainer2/VBoxContainer/IsGetListSupported
-@onready var is_native_popup_supported_label = $MarginContainer2/VBoxContainer/IsNativePopupSupported
 @onready var achievement_key_input = $MarginContainer2/VBoxContainer/HBoxContainer2/AchievementKeyInput
 @onready var achievement_name_input = $MarginContainer2/VBoxContainer/HBoxContainer2/AchievementNameInput
 
 func _ready():
 	is_achievements_supported_label.text = "Is Achievements Supported: " + str(Bridge.achievements.is_supported)
-	is_get_list_supported_label.text = "Is Get List Supported: " + str(Bridge.achievements.is_get_list_supported)
-	is_native_popup_supported_label.text = "Is Native Popup Supported: " + str(Bridge.achievements.is_native_popup_supported)
 
 func _on_unlock_button_pressed():
 	# Platform-specific data is resolved from the achievements
@@ -33,10 +29,3 @@ func _on_get_list_completed(success, list):
 		print("name:" + str(item.name))
 		print("description:" + str(item.description))
 		print("unlocked:" + str(item.unlocked))
-
-
-func _on_show_native_popup_button_pressed():
-	Bridge.achievements.show_native_popup(Callable(self, "_on_show_native_popup_completed"))
-
-func _on_show_native_popup_completed(success):
-	print(success)
